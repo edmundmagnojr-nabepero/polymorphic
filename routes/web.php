2921efe75de7687d39ea20bb1cc35616701c9967
edmundmagnojr-nabepero <edmund.magno@nabepero.co.jp>
2022-12-12 15:04:17 +0800

    finished polymorphic relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Photo;
+use App\Models\Product;
+use App\Models\Staff;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,39 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+    $staff = Staff::find(1);
+    $staff->photos()->create(['path'=>'example.jpg']);
+});
+
+Route::get('/read', function(){
+    $staff = Staff::findOrFail(1);
+    foreach($staff->photos as $photo){
+        echo $photo->path;
+    }
+});
+
+Route::get('/update', function(){
+    $staff = Staff::findOrFail(1);
+    $photo = $staff->photos()->whereId(1)->first();
+    $photo->path = "Update example.jpg";
+    $photo->save();
+});
+
+Route::get('/delete', function(){
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(1)->delete();
+});
+
+Route::get('/assign', function(){
+    $staff = Staff::findOrFail(1);
+    $photo = Photo::findOrFail(2);
+    $staff->photos()->save($photo);
+});
+
+Route::get('/un-assign', function(){
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(2)->update(['imageable_id'=>'0', 'imageable_type'=>'']);
 });
